aed60986772d3a9443949e3ab953730c92b07cc9
AswinChristo <aswinj2019@srishakthi.ac.in>
2019-06-09 19:54:34 +0530

    Updates

---
+++ precache-manifest.2d54113932f26d20437badbeb3e8fe04.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c1030c2529f9f43ddf59a751b867702",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48e0990e1ee68246f6d7",
+    "url": "/static/css/main.3d8bb6a7.chunk.css"
+  },
+  {
+    "revision": "bd957a12fbaca2b11132",
+    "url": "/static/js/2.30403b4f.chunk.js"
+  },
+  {
+    "revision": "48e0990e1ee68246f6d7",
+    "url": "/static/js/main.b7189e79.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f13472d6a66f3d2e176dfda1b86cfea9",
+    "url": "/static/media/botPaper.f13472d6.png"
+  },
+  {
+    "revision": "697f18ae4c61e449000ccea82cde7518",
+    "url": "/static/media/botRock.697f18ae.png"
+  },
+  {
+    "revision": "98de83ead69f9ad1cb49929935f5421f",
+    "url": "/static/media/botScissor.98de83ea.png"
+  },
+  {
+    "revision": "9871e996b7abb30ea92888441352266b",
+    "url": "/static/media/nyan-cat.9871e996.gif"
+  },
+  {
+    "revision": "2e9d9c695d34fac0444768ada3830133",
+    "url": "/static/media/playerPaper.2e9d9c69.png"
+  },
+  {
+    "revision": "6dd82a3938fe4bc83f894722a38a4786",
+    "url": "/static/media/playerRock.6dd82a39.png"
+  },
+  {
+    "revision": "1ff1e6948394d898266707ed2d81fcd8",
+    "url": "/static/media/playerScissor.1ff1e694.png"
+  }
+]);
